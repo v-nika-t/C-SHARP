ff5c677e387044234c8d54166eaec90922f9713f
Tsishalovich Veranika <vt3199662@gmail.com>
2022-12-03 16:31:33 +0300

    Done task_13, task_15

--- homework_2/homework_tasks/task_13/Program.cs
+++ homework_2/homework_tasks/task_13/Program.cs
@@ -1,7 +1,31 @@
 ﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-
 // 645 -> 5
-
 // 78 -> третьей цифры нет
-
 // 32679 -> 6
+
+int number;
+
+do {
+    Console.WriteLine("Введите число до 8 знаков");
+    number = Convert.ToInt32(Console.ReadLine());
+    if(number > 100000000) {
+        Console.WriteLine("Вы ввели число больше 8 знаков");
+        continue;
+    }
+} while ( number > 100000000);
+
+
+while(number != 0) 
+{
+        if(number / 100 == 0)  {
+            Console.WriteLine("Третьей цифры нет");
+            break;
+        } 
+        if(number / 1000 == 0) {
+            Console.WriteLine($"Третья цифра {number % 10}");
+            break;
+        }
+        number = number / 10;
+}
+
+
--- homework_2/homework_tasks/task_15/Program.cs
+++ homework_2/homework_tasks/task_15/Program.cs
@@ -3,3 +3,23 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+string[] daysOfTheWeek = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"} ;
+int day;
+int index = 0;
+bool isDayOff=false;
+do {
+    Console.WriteLine("Введите число дня недели от 1 до 7");
+    day = Convert.ToInt32(Console.ReadLine());
+    if(day > 8 || day < 0) {
+        Console.WriteLine("Вы ввели не верное число");
+        continue;
+    } 
+    index = day -1;
+    if(index == 5 || index == 6 ) isDayOff = true;
+    string text = isDayOff ? "выходной" : "будний";
+    Console.WriteLine($"{daysOfTheWeek[index]} - это {text}");
+
+} while ( day > 8 || day < 0);
+
+
