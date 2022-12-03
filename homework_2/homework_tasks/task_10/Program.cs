4fed5ad3d25d8385a199425edfd64f74092fecef
Tsishalovich Veranika <vt3199662@gmail.com>
2022-12-03 14:50:30 +0300

    Done task_10

---
+++ homework_2/homework_tasks/task_10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10: Напишите программу, 
+// которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+int number;
+do {
+    Console.WriteLine("Введите трехзначное число");
+    number = Convert.ToInt32(Console.ReadLine());
+    if(number / 1000 != 0) Console.WriteLine("Вы ввели не трехзначное число");
+} while ( number / 1000 != 0 );
+
+number = (number / 10 ) % 10 ;
+Console.WriteLine($"Второе число:  {number}");
+
