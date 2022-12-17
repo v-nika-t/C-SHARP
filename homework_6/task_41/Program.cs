cfa3095cc9e5fbded57b63bf23cd7440bf5a407d
Tsishalovich Veranika <vt3199662@gmail.com>
2022-12-17 15:59:52 +0300

    Done homework_6

---
+++ homework_6/task_41/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//  Ввод чисел останавливается 
+//  при помощи ввода слова "stop"
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 4
+
+
+int quantity = 0;
+ Console.WriteLine("Введите число. Если хотите узнать кол-во введите stop");
+ do {
+    Console.WriteLine("Введите число. Если хотите узнать кол-во введите stop");
+    string? result = Console.ReadLine();
+    if(result == "stop") {
+        Console.WriteLine(quantity);
+        break;
+    }
+    int number = Convert.ToInt32(result);
+    if(number > 0) quantity++;
+
+} while (true);
+ 
+
---
+++ homework_6/task_43/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку 
+// пересечения двух прямых, 
+// заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2;
+//  значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int[] arrayParams = new int [4];
+string[] arrayTitle = {"b1", "b2", "k1", "k2"};
+int number = 0;
+int i = 0;
+do {
+    Console.WriteLine($"Введите {arrayTitle[i]}");
+    number = Convert.ToInt32(Console.ReadLine());
+    arrayParams[i] = number;
+    i++;
+} while (i < 4);
+
+for (int k = 0 ; k < arrayParams.Length ; k++) {
+  Console.WriteLine(arrayParams[k]);
+}
+
+
+double x =  Convert.ToDouble(arrayParams[1] - arrayParams[0]) / Convert.ToDouble(arrayParams[2] - arrayParams[3]);
+double y = (arrayParams[3] * x) + arrayParams[1]; 
+Console.WriteLine($"Координаты: ({x}, {y})");
